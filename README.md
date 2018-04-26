91e8fc14563c23f0a48fed30f5ff4629ad620672
Jeadie <jack.eadie0@gmail.com>
2018-04-26 20:14:40 +1000

    project structure and basic architecture

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # solid-barnacle
-Step file stub automation for Python's behave BDD framework 
+Step file automation for gherkin feature files for Python's behave BDD framework.
+
+## About
+solid-barnacle is a tool to auto-generate scenario files from gherkin DSL files. Although this tool
+is specifically designed for Python's behave, the formatters can extend to other step file stubs like jBehave.
+
+solid-barnacle can update the required step files when changes occur in the gherkin files.
+
---
+++ SolidBarnacleAPI.py
@@ -0,0 +1,14 @@
+
+class SolidBarnacleAPI(object):
+
+    @staticmethod
+    def translate_features(feature_path, step_path=None):
+        """
+        Translates all the feature files into step files. Duplicate method will be
+        constructed for lines in features that are in different files.
+        (i.e. feature files are considered independently.)
+        :param feature_path: String path the the feature folder
+        :param step_path: String path to the desired folder for step files. If None,
+            will be placed in a new folder "../step" relative to the feature_path
+        """
+        pass
